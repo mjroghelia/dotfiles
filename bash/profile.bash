cc7371b1781d99c2be11ba0402678258aca5bc65
Mark Roghelia <mark@roboshark.com>
2021-10-10 19:29:00 -0400

    bash completion update

--- bash/profile.bash
+++ bash/profile.bash
@@ -1,10 +1,12 @@
 source ~/.env
-source $ETC_DIR/bash/project.bash
 
-#alias ss='svn stat'
+if [ -n "$ETC_DIR" ]
+then
+  source $ETC_DIR/bash/project.bash
+fi
+
 alias ss='git status -s'
 alias be='bundle exec'
-alias ls='ls -G'
 
 # Don't overwrite the bash history when I leave a session.  This plays better with multiple concurrent sessions
 shopt -s histappend
@@ -29,9 +31,20 @@ if [ "$ETC_PLATFORM" == "mac" ]; then
   stty -ixon
 
   # Git command completion - brew install bash-completion
-  if [ -f `brew --prefix`/etc/bash_completion ]; then
-    . `brew --prefix`/etc/bash_completion
+  if type brew &>/dev/null
+  then
+    HOMEBREW_PREFIX="$(brew --prefix)"
+    if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]
+    then
+      source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
+    else
+      for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*
+      do
+        [[ -r "${COMPLETION}" ]] && source "${COMPLETION}"
+      done
+    fi
   fi
+
 fi
 
 if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
