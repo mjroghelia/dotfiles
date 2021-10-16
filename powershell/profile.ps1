b08b8cabe27890fe1a66490c5c53aaf4f77baf53
Mark Roghelia <mark@roboshark.com>
2021-10-16 00:48:43 -0400

    Add gh completion and remove less alias on Unix

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -2,13 +2,16 @@ Import-Module PSReadLine
 Set-PSReadLineOption -EditMode Emacs -BellStyle None
 
 function ss { git status } 
-Set-Alias less more 
 
 $env:LESSCHARSET="utf-8"
 
 if ($IsWindows -Or $env:OS -eq "Windows_NT") {
+    Set-Alias less more 
     $env:PYTHONPATH = (Join-Path $HOME "src/etc/python")
 }
 elseif ($IsMacOS) {
     $env:PATH += ":/usr/local/bin"
 }
+
+# Add GitHub cli completions
+Invoke-Expression -Command $(gh completion -s powershell | Out-String)
