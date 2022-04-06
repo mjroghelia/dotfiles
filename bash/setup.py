58f3de1a3463c7db4ba19a23af796d70343d0d9a
Mark Roghelia <mark@roboshark.com>
2022-04-06 01:50:59 -0400

    Split out bashrc

--- bash/setup.py
+++ bash/setup.py
@@ -16,6 +16,10 @@ if (not env_file.exists()):
         f.write('ETC_WORK={}\n'.format(str(etc.is_work()).lower()))
 
 if (not etc.is_windows()):
+    rc_src = etc.script_dir() / "bashrc"
+    print("Linking .bashrc to {}...".format(rc_src))
+    etc.update_symlink(rc_src, etc.home() / ".bashrc")
+
     profile_src = etc.script_dir() / "profile.bash"
     print("Linking .bash_profile to {}...".format(profile_src))
     etc.update_symlink(profile_src, etc.home() / ".bash_profile")
